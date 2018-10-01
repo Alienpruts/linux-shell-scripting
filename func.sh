3feb05cb1653fb0494f23a83e59593f70c5a2a03
Bert Van de Casteele (Alienpruts) <vandecasteele@evilones.be>
2018-10-01 10:10:59 +0200

    Added parameters to simple function script

--- func.sh
+++ func.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
 #Two ways of declaring functions, with or without function keyword
-
 function Hello() {
-    echo "Hello"
+    # One way to use local parameter : set the value in a local variable
+    local LNAME=$1
+    echo "Hello $LNAME"
 }
 
 Goodbye() {
-    echo "Goodbye"
+    # Second way to use local parameter : just reference it like in a non function script
+    echo "Goodbye $1"
 }
 
 echo "Calling the hello function"
-Hello
+Hello Bob
 echo "Calling the goodbye function"
-Goodbye
+Goodbye Robert
 
 exit 0
